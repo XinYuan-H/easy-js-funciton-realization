77ff00e6f8279179372b36fd0217f58012f305db
XinYuan-H <116531930+XinYuan-H@users.noreply.github.com>
2023-02-24 01:07:33 +0800

    Add files via upload

---
+++ 滾動協議閱讀.js
@@ -0,0 +1,19 @@
+// onscroll 滾動事件  當用戶滾動
+
+let info = document.getElementById("info");
+let agree = document.getElementById("agree");
+let submit = document.getElementById("submit");
+info.onscroll = function(){
+    if(this.scrollHeight - Math.floor(this.scrollTop) == this.clientHeight){
+        //scrollTop在google裡會有浮點數 因此要用Math來將值取整
+        //disabled 返回true為禁用 false為不禁用
+           agree.disabled = false;    
+    };
+};
+
+
+agree.onclick = function(){
+    if(agree.checked){
+    submit.disabled = false;};
+};
+
